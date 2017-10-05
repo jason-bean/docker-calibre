0c90f7b6613ea2b5eb8b64ec124337d007368478
Jason Bean <taddeusz@gmail.com>
2017-10-05 18:45:23 -0500

    specify log

--- calibre-server.sh
+++ calibre-server.sh
@@ -2,7 +2,7 @@
 
 URL_PREFIX=${URL_PREFIX}
 if [ -n "$URL_PREFIX" ]; then
-  /opt/calibre/calibre-server --daemonize "/calibre-library"
+  /opt/calibre/calibre-server --daemonize --log=/dev/stdout "/calibre-library"
 else
-  /opt/calibre/calibre-server --daemonize --url-prefix="$URL_PREFIX" "/calibre-library"
+  /opt/calibre/calibre-server --daemonize --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
 fi
