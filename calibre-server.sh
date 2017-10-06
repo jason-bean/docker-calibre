7e9c82469553b2e17e22fb36fedeb50a9fd013e3
Jason Bean <taddeusz@gmail.com>
2017-10-06 09:44:07 -0500

    test

--- calibre-server.sh
+++ calibre-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 URL_PREFIX=${URL_PREFIX}
-if [ -n "$URL_PREFIX" ]; then
-  /opt/calibre/calibre-server --daemonize --log=/dev/stdout "/calibre-library"
-else
+#if [ -n "$URL_PREFIX" ]; then
+#  /opt/calibre/calibre-server --daemonize --log=/dev/stdout "/calibre-library"
+#else
   /opt/calibre/calibre-server --daemonize --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
-fi
+#fi
