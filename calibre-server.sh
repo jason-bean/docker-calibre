457106ec1a73fed058ef2ad6bc4a30ba66c297be
Jason Bean <taddeusz@gmail.com>
2017-10-09 13:59:17 -0500

    Test remove daemonize

--- calibre-server.sh
+++ calibre-server.sh
@@ -4,5 +4,5 @@ URL_PREFIX=${URL_PREFIX}
 #if [ -n "$URL_PREFIX" ]; then
 #  /opt/calibre/calibre-server --daemonize --log=/dev/stdout "/calibre-library"
 #else
-  /opt/calibre/calibre-server --daemonize --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
+  /opt/calibre/calibre-server --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
 #fi
