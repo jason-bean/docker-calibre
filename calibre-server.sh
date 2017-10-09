9b74e9a07aab8bf4ab2d0589da62b3a66dc278cb
Jason Bean <taddeusz@gmail.com>
2017-10-09 15:46:53 -0500

    Calibre server enable local write

--- calibre-server.sh
+++ calibre-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 URL_PREFIX=${URL_PREFIX}
-/opt/calibre/calibre-server --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
+/opt/calibre/calibre-server --enable-local-write --log=/dev/stdout --url-prefix="$URL_PREFIX" "/calibre-library"
